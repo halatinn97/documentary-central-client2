ac9ae2bcbe3c29fa293789d2ac84efe7cbbe7d30
Hala Tinn <hala79tinn@gmail.com>
2022-07-07 00:40:56 +0200

    Update registration-view.jsx

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
+
+import './registration-view.scss';
+import axios from 'axios'
+
+export function RegistrationView(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [email, setEmail] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('https://documentary-central.herokuapp.com/documentaries', {
+            Username: username,
+            Password: password,
+            Email: email
+        })
+            .then(response => {
+                const data = response.data;
+                console.log(data);
+                window.open('/', '_self');
+            })
+
+            .catch(error => {
+                console.log(error);
+                alert('Something was not entered right.')
+            });
+    };
+
+    return (
+        <Form>
+            <Form.Group>
+                <Form.Label>Username:  </Form.Label>
+                <Form.Control
+                    type="text"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    required
+                    placeholder="Enter a username"
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Password:  </Form.Label>
+                <Form.Control
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder="Enter a password"
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Email: </Form.Label>
+                <Form.Control
+                    type="text"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder="Enter an email"
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Birthday: </Form.Label>
+                <Form.Control
+                    type="text"
+                    value={birthday}
+                    onChange={e => setBirthday(e.target.value)}
+                    placeholder="Enter a birthday"
+                />
+            </Form.Group>
+
+            <Button variant="primary" type="submit"
+                onClick={handleSubmit}>Submit</Button>
+        </Form>
+    );
+}
+
+RegistrationView.propTypes = {
+    onRegistered: PropTypes.func.isRequired,
+};
