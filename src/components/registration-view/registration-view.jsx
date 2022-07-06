2e05e446e11fae9d9b013eeb071e5455b2bc2788
Hala Tinn <hala79tinn@gmail.com>
2022-07-07 01:15:11 +0200

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -14,10 +14,11 @@ export function RegistrationView(props) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post('https://documentary-central.herokuapp.com/documentaries', {
+        axios.post('https://documentary-central.herokuapp.com/users', {
             Username: username,
             Password: password,
-            Email: email
+            Email: email,
+            Birthday: birthday
         })
             .then(response => {
                 const data = response.data;
@@ -32,51 +33,68 @@ export function RegistrationView(props) {
     };
 
     return (
-        <Form>
-            <Form.Group>
-                <Form.Label>Username:  </Form.Label>
-                <Form.Control
-                    type="text"
-                    value={username}
-                    onChange={e => setUsername(e.target.value)}
-                    required
-                    placeholder="Enter a username"
-                />
-            </Form.Group>
+        <Container>
+            <Row>
+                <Col>
+                    <CardGroup>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Register now to view unlimited documentaries, anywhere, anytime.</Card.Title>
+                                <Form>
+                                    <Form.Group>
+                                        <Form.Label>Username:  </Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            value={username}
+                                            onChange={e => setUsername(e.target.value)}
+                                            required
+                                            placeholder="Enter a username"
+                                        />
+                                    </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Password:  </Form.Label>
-                <Form.Control
-                    type="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    placeholder="Enter a password"
-                />
-            </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Password:  </Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            value={password}
+                                            onChange={e => setPassword(e.target.value)}
+                                            required
+                                            minLength="8"
+                                            placeholder="Enter a password"
+                                        />
+                                    </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Email: </Form.Label>
-                <Form.Control
-                    type="text"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    placeholder="Enter an email"
-                />
-            </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Email: </Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            value={email}
+                                            onChange={e => setEmail(e.target.value)}
+                                            required
+                                            placeholder="Enter an email"
+                                        />
+                                    </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Birthday: </Form.Label>
-                <Form.Control
-                    type="text"
-                    value={birthday}
-                    onChange={e => setBirthday(e.target.value)}
-                    placeholder="Enter a birthday"
-                />
-            </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Birthday: </Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            value={birthday}
+                                            onChange={e => setBirthday(e.target.value)}
+                                            placeholder="Enter a birthday"
+                                        />
+                                    </Form.Group>
 
-            <Button variant="primary" type="submit"
-                onClick={handleSubmit}>Submit</Button>
-        </Form>
+                                </Form>
+                                <Button variant="primary" type="submit"
+                                    onClick={handleSubmit}>Submit</Button>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
+
     );
 }
 
