ffc831066403ce00c021e83d22a5e1ffe838ba76
Hala Tinn <hala79tinn@gmail.com>
2022-07-06 23:07:15 +0200

    Update documentary-card.jsx

--- src/components/documentary-card/documentary-card.jsx
+++ src/components/documentary-card/documentary-card.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 
 export class DocumentaryCard extends React.Component {
     render() {
+
         const { documentary, onDocumentaryClick } = this.props;
-        return <div className="documentary-card" onClick={() => { onDocumentaryClick(documentary); }}>{documentary.Title}</div>;
+
+        return (
+            <Card>
+                <Card.Img variant="top" src={documentary.ImagePath} />
+                <Card.Body>
+                    <Card.Title>{documentary.Title}</Card.Title>
+                    <Card.Text>{documentary.Description}</Card.Text>
+                    <Button onClick={() => onDocumentaryClick(documentary)} variant="link">Open</Button>
+                </Card.Body>
+            </Card>
+        );
     }
-}
+}    
