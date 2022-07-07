40440a7960afad3f2f380f3075a60e8d5b4b1182
Hala Tinn <hala79tinn@gmail.com>
2022-07-07 16:46:40 +0200

    Update index.jsx
    - Due to commit error: re-commit newest React version replacing ReactDOM.render with createRoot

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
-
-// Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
 
 // Main component (will eventually use all the others)
 class DocumentaryCentralApplication extends React.Component {
@@ -17,8 +16,10 @@ class DocumentaryCentralApplication extends React.Component {
     }
 }
 
-// Finds the root of your app
+// Finds the root of app
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(DocumentaryCentralApplication), container);
+const root = ReactDOM.createRoot(container);
+
+root.render(<DocumentaryCentralApplication />);
+
