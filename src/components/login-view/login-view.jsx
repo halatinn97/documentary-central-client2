2550ca9b937d1eb86e87340ad8af370375f2b94f
Hala Tinn <hala79tinn@gmail.com>
2022-07-09 18:16:39 +0200

    Add form validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -10,22 +10,46 @@ export function LoginView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const [usernameErr, setUsernameErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+
+    const validate = () => {
+        let isReq = true;
+        if (!username) {
+            setUsernameErr('Username Required');
+            isReq = false;
+        } else if (username.length < 2) {
+            setUsernameErr('Username must be 2 characters long');
+            isReq = false;
+        }
+        if (!password) {
+            setPasswordErr('Password Required');
+            isReq = false;
+        } else if (password.length < 6) {
+            setPassword('Password must be 6 characters long');
+            isReq = false;
+        }
+
+        return isReq;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios
-            .post('https://documentary-central.herokuapp.com/login', {
-                Username: username,
-                Password: password,
-            })
-            .then(response => {
-                const data = response.data;
-                props.onLoggedIn(data);
-            })
-            .catch(error => {
-                console.log(error);
-                alert('User does not exist.')
-
-            });
+        const isReq = validate();
+        if (isReq) {
+            axios
+                .post('https://documentary-central.herokuapp.com/login', {
+                    Username: username,
+                    Password: password,
+                })
+                .then(response => {
+                    const data = response.data;
+                    props.onLoggedIn(data);
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        }
     };
 
 
@@ -35,11 +59,13 @@ export function LoginView(props) {
             <Form.Group controlId="formUsername">
                 <Form.Label>Username:</Form.Label>
                 <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                {usernameErr && <p>{usernameErr}</p>}
             </Form.Group>
 
             <Form.Group controlId="formPassword">
                 <Form.Label>Password:</Form.Label>
                 <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                {passwordErr && <p>{passwordErr}</p>}
             </Form.Group>
             <Button variant="primary" type="submit" onClick={handleSubmit}>
                 Submit
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 import './registration-view.scss';
 
 
@@ -11,95 +12,145 @@ export function RegistrationView(props) {
     const [birthday, setBirthday] = useState('');
     const [email, setEmail] = useState('');
 
+    const [usernameErr, setUsernameErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+    const [emailErr, setEmailErr] = useState('');
+
+
+
+
+    const validate = () => {
+        let isReq = true;
+        if (!username) {
+            setUsernameErr('Username is required');
+            isReq = false;
+        } else if (username.length < 5) {
+            setUsernameErr('Username must be at least 5 characters long');
+            isReq = false;
+        }
+        if (!password) {
+            setPasswordErr('Password is required');
+            isReq = false;
+        } else if (password.length < 5) {
+            setPasswordErr('Password must be at least 5 characters long');
+            isReq = false;
+        }
+        if (!email) {
+            setEmailErr('Email is required');
+            isReq = false;
+        } else if (email.indexOf("@") === -1) {
+            setEmailErr('Email is invalid');
+            isReq = false;
+        }
+
+        return isReq;
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post('https://documentary-central.herokuapp.com/users', {
-            Username: username,
-            Password: password,
-            Email: email,
-            Birthday: birthday
-        })
-            .then(response => {
-                const data = response.data;
-                props.onRegistered(data)
-                /*console.log(data);*/
-                window.open('/', '_self');
+        const isReq = validate();
+        if (isReq) {
+
+            axios.post('https://documentary-central.herokuapp.com/users', {
+                Username: username,
+                Password: password,
+                Email: email,
+                Birthday: birthday
             })
+                .then(response => {
+                    const data = response.data;
+                    props.onRegistered(data)
+                    /*console.log(data);*/
+                    window.open('/', '_self');
+                })
 
-            .catch(error => {
-                console.log(error);
-                alert('Something was not entered right.')
-            });
-    };
+                .catch(error => {
+                    console.log(error);
+                    alert('Something was not entered right.')
+                });
+        };
 
 
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    <CardGroup>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Register now to view unlimited documentaries, anywhere, anytime.</Card.Title>
-                                <Form>
-                                    <Form.Group>
-                                        <Form.Label>Username:  </Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            value={username}
-                                            onChange={e => setUsername(e.target.value)}
-                                            required
-                                            placeholder="Enter a username"
-                                        />
-                                    </Form.Group>
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <CardGroup>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title>Register now to view unlimited documentaries, anywhere, anytime.</Card.Title>
+                                    <Form>
+                                        <Form.Group controlId="formUsername">
+                                            <Form.Label>Username:  </Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                value={username}
+                                                onChange={e => setUsername(e.target.value)}
+                                                required
+                                                placeholder="Enter a username"
+                                            />
+                                            {usernameErr && <p>{usernameErr}</p>}
 
-                                    <Form.Group>
-                                        <Form.Label>Password:  </Form.Label>
-                                        <Form.Control
-                                            type="password"
-                                            value={password}
-                                            onChange={e => setPassword(e.target.value)}
-                                            required
-                                            minLength="8"
-                                            placeholder="Enter a password"
-                                        />
-                                    </Form.Group>
+                                        </Form.Group>
 
-                                    <Form.Group>
-                                        <Form.Label>Email: </Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            value={email}
-                                            onChange={e => setEmail(e.target.value)}
-                                            required
-                                            placeholder="Enter an email"
-                                        />
-                                    </Form.Group>
+                                        <Form.Group controlId="formPassword">
+                                            <Form.Label>Password:  </Form.Label>
+                                            <Form.Control
+                                                type="password"
+                                                value={password}
+                                                onChange={e => setPassword(e.target.value)}
+                                                required
+                                                minLength="8"
+                                                placeholder="Enter a password"
+                                            />
+                                            {passwordErr && <p>{passwordErr}</p>}
+                                        </Form.Group>
 
-                                    <Form.Group>
-                                        <Form.Label>Birthday: </Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            value={birthday}
-                                            onChange={e => setBirthday(e.target.value)}
-                                            placeholder="Enter a birthday"
-                                        />
-                                    </Form.Group>
+                                        <Form.Group controlId="formEmail">
+                                            <Form.Label>Email: </Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                value={email}
+                                                onChange={e => setEmail(e.target.value)}
+                                                required
+                                                placeholder="Enter an email"
+                                            />
+                                            {emailErr && <p>{emailErr}</p>}
 
-                                </Form>
-                                <Button variant="primary" type="submit"
-                                    onClick={handleSubmit}>Submit</Button>
-                            </Card.Body>
-                        </Card>
-                    </CardGroup>
-                </Col>
-            </Row>
-        </Container>
+                                        </Form.Group>
 
-    );
+                                        <Form.Group controlId="formBirthday">
+                                            <Form.Label>Birthday: </Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                value={birthday}
+                                                onChange={e => setBirthday(e.target.value)}
+                                                placeholder="Enter a birthday"
+                                            />
+                                        </Form.Group>
+                                        <Button variant="primary" type="submit"
+                                            onClick={handleSubmit}>Submit</Button>
+                                        <p></p>
+                                        <p>Already registered <Link to={'/'}>Sign in</Link>here</p>
+                                    </Form>
+
+                                </Card.Body>
+                            </Card>
+                        </CardGroup>
+                    </Col>
+                </Row>
+            </Container>
+
+        );
+    }
 }
 
+/*
 RegistrationView.propTypes = {
-    onRegistered: PropTypes.func.isRequired,
+    Onregister: propTypes.shape({
+        Username: PropTypes.string.isRequired,
+        Password: PropTypes.string.isRequired,
+        Email: PropTypes.string.isRequired
+    }),
 };
+*/
