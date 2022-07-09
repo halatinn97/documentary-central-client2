640b8f2bb97031ffa42c8726d7e9ccfc66789977
Hala Tinn <hala79tinn@gmail.com>
2022-07-09 21:40:09 +0200

    Update main-view with routing

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { DocumentaryCard } from '../documentary-card/documentary-card';
 import { DocumentaryView } from '../documentary-view/documentary-view';
-import Row from 'react-bootstrap/Row';
+import { GenreView } from '../genre-view/genre-view';
+import { PersonalityView } from '../personality-view/personality-view';
+import { ProfileView } from '../profile-view/profile-view';
+import { Row, Col } from 'react-bootstrap';
 import './main-view.scss';
 
 
@@ -22,17 +26,14 @@ export class MainView extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('https://documentary-central.herokuapp.com/documentaries')
-            .then(response => {
-                this.setState({
-                    documentaries: response.data
-                });
-            })
-            .catch(error => {
-                console.log(error);
+        let accessToken = localStorage.getItem('token');
+        if (accessToken !== null) {
+            this.setState({
+                user: localStorage.getItem('user')
             });
+            this.getDocumentaries(accessToken);
+        }
     }
-
     setSelectedDocumentary(newSelectedDocumentary) {
         this.setState({
             selectedDocumentary: newSelectedDocumentary
@@ -45,38 +46,95 @@ export class MainView extends React.Component {
         });
     }
 
-    onLoggedIn(user) {
+    onLoggedIn(authData) {
+        console.log(authData);
         this.setState({
-            user
+            user: authData.user.Username
+        });
+
+        localStorage.setItem('token', authData.token);
+        localStorage.setItem('user', authData.user.Username);
+        this.getDocumentaries(authData.token);
+    }
+
+    onLoggedOut() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        this.setState({
+            user: null
         });
     }
 
+    getDocumentaries(token) {
+        axios.get('https://documentary-central.herokuapp.com/documentaries', {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+            .then(response => {
+                // Assign the result to the state
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     render() {
-        const { documentaries, selectedDocumentary, user, registered } = this.state;
-
-        /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
-        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
-
-        if (!registered) return <RegistrationView onRegistered={register => this.onRegistered(register)} />;
-
-        // Before the movies have been loaded
-        if (documentaries.length === 0) return <div className="main-view" />;
+        const { documentaries, user } = this.state;
 
         return (
-            <Row className="main-view justify-content-md-center">
-                {selectedDocumentary
-                    ? (
-                        <Col md={8}>
-                            <DocumentaryView documentary={selectedDocumentary} onBackClick={newSelectedDocumentary => { this.setSelectedDocumentary(newSelectedDocumentary); }} />
+            <Router>
+                <Row className="main-view justify-content-md-center">
+                    <Route exact path="/" render={() => {
+                        if (!user) return <Col>
+                            <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
                         </Col>
-                    )
-                    : documentaries.map(documentary => (
-                        <Col md={3}>
-                            <DocumentaryCard key={documentary._id} documentary={documentary} onDocumentaryClick={newSelectedDocumentary => { this.setSelectedDocumentary(newSelectedDocumentary); }} />
+                        if (documentaries.length === 0) return <div className="main-view" />;
+                        return documentaries.map(documentary => (
+                            <Col md={3} key={documentary._id}>
+                                <DocumentaryCard documentary={documentary} />
+                            </Col>
+                        ))
+                    }} />
+                    <Route path="/register" render={() => {
+                        if (user) return <Redirect to="/" />
+                        return <Col>
+                            <RegistrationView />
                         </Col>
-                    ))
-                }
-            </Row>
+                    }} />
+                    <Route path="/documentaries/:DocumentaryId" render={({ match, history }) => {
+                        if (documentaries.length === 0) return <div className="main-view" />;
+                        return <Col md={8}>
+                            <DocumentaryView documentary={documentaries.find(documentary => documentary._id === match.params.DocumentaryId)} onBackClick={() => history.goBack()} />
+                        </Col>
+                    }} />
+                    <Route path="/documentaries/genre/:Name" render={({ match, history }) => {
+                        if (documentaries.length === 0) return <div className="main-view" />;
+
+                        return <Col md={8}>
+                            <GenreView genre={documentaries.find(m => m.Genre.Name === match.params.name)} onBackClick={() => history.goBack()} />
+                        </Col>
+                    }} />
+                    <Route path="documentaries/featuredPersonalities/:name" render={({ match, history }) => {
+                        if (documentaries.length === 0) return <div className="main-view" />;
+                        return <Col md={8}>
+                            <PersonalityView featuredPersonality={documentaries.find(documentary => documentary.featuredPersonality.Name === match.params.name).featuredPersonality} onBackClick={() => history.goBack()} />
+                        </Col>
+                    }} />
+                    <Route path="/users/:Username" render={({ history }) => {
+                        if (!user) return <Col>
+                            <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                        </Col>
+                        if (documentaries.length === 0) return <div className="main-view" />;
+                        return <Col md={8}>
+                            <ProfileView documentaries={documentaries} user={user} onBackClick={() => history.goBack()} />
+                        </Col>
+                    }} />
+
+                </Row>
+            </Router>
+
         );
     }
 }
