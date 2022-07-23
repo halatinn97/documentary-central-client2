d3f3b41bef81d24e814d72bb13d69b1382966f04
Hala Tinn <hala79tinn@gmail.com>
2022-07-23 05:07:25 +0200

    Update to match Routing branch

--- src/components/documentary-view/documentary-view.jsx
+++ src/components/documentary-view/documentary-view.jsx
@@ -3,9 +3,48 @@ import PropTypes from 'prop-types';
 import { Button, Container, Row, Col, Card, CardGroup } from 'react-bootstrap';
 import './documentary-view.scss';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
 
 
 export class DocumentaryView extends React.Component {
+
+
+    //Remove docu from favorites
+    addFav = (documentaryId) => {
+        const token = localStorage.getItem('token');
+        const user = localStorage.getItem('user');
+
+        axios.post(`https://documentary-central.herokuapp.com/users/${user}/documentaries/${documentaryId}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+            .then(() => {
+                alert('The documentary has been deleted from your favorites list.')
+                window.open(`/users/${user}`, '_self');
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+
+
+    //Remove docu from favorites
+    removeFav = (documentaryId) => {
+        const token = localStorage.getItem('token');
+        const user = localStorage.getItem('user');
+        axios.delete(`https://documentary-central.herokuapp.com/users/${user}/documentaries/${documentaryId}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+            .then(() => {
+                alert('The documentary has been deleted from your favorites list.')
+                window.open(`/users/${user}`, '_self');
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+
 
     render() {
         const { documentary, user } = this.props;
@@ -69,6 +108,18 @@ export class DocumentaryView extends React.Component {
                         <Link to={`users/${user}/documentaries/${documentary._id}`}>
                             <Button variant="link">Add to 💙</Button>
                         </Link>
+                        <Button
+                            className="add-favorites ml-2 my-2"
+                            onClick={() => this.addFav(documentary._id)}
+                        >
+                            Add to Favorites
+            </Button>
+                        <Button
+                            className="remove-favorites ml-2"
+                            onClick={() => this.removeFav(documentary._id)}
+                        >
+                            Remove from Favorites
+            </Button>
                     </Card.Body>
                 </Card>
             </Container>
@@ -77,7 +128,7 @@ export class DocumentaryView extends React.Component {
     }
 }
 
-/*
+
 DocumentaryView.propTypes = {
     documentary: PropTypes.shape({
         Title: PropTypes.string.isRequired,
@@ -96,4 +147,3 @@ DocumentaryView.propTypes = {
         }),
     }).isRequired
 };
-*/
