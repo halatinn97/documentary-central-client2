07bc5387c0dd693926a8b88df5b3b4e7bbbd917a
Hala Tinn <hala79tinn@gmail.com>
2022-07-23 04:47:03 +0200

    Update index.jsx with Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { MainView } from './components/main-view/main-view';
+import MainView from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import DocumentariesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import './index.scss';
+
+const store = createStore(DocumentariesApp, devToolsEnhancer());
 
 
 // Main component (will eventually use all the others)
 class DocumentaryCentralApplication extends React.Component {
     render() {
         return (
-            <Container>
-                <MainView />
-            </Container>
+            <Provider store={store}>
+                <Container>
+                    <MainView />
+                </Container>
+            </Provider>
         );
     }
 }
